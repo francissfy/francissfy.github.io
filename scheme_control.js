c19bee1ad0b7fe4b5e7a4ee11a1d54f5b7ebbf04
francissfy <30970977+francissfy@users.noreply.github.com>
2018-12-01 12:22:25 +0800

    functions to draw event cell

--- scheme_control.js
+++ scheme_control.js
@@ -9,11 +9,11 @@ function initTimeData(){
 
 
 function genrateDateTag(document,mDate,color){//color is string
-    var height = 65;
-    var width = 140;
-    const textSizeNotToday = 27;
+    var height = 60;
+    var width = 155;
+    const textSizeNotToday = 32;
     const textFontNotToday = textSizeNotToday+"px PingFang SC";
-    const textSizeToday = 20;
+    const textSizeToday = 26;
     const textFontToday = textSizeToday+"px PingFang SC";
     var radius = 12;
     //
@@ -70,16 +70,89 @@ function genrateDateTag(document,mDate,color){//color is string
         mContext.font = textFontToday;
         mContext.fillStyle = color;
         mContext.textAlign = "center";
-        mContext.fillText(day+". "+date,width/2,height/4+textSizeToday/2);
+        mContext.fillText(day+". "+date,width/2,(1-0.1)*height/4+textSizeToday/2);
         mContext.fillStyle = "#FFF";
-        mContext.fillText("Today",width/2,(3-0.2)*height/4+textSizeToday/2);
+        mContext.fillText("Today",width/2,(3-0.35)*height/4+textSizeToday/2);//compensate;
     }else{
         mContext.font = textFontNotToday;
         mContext.fillStyle = color;
         mContext.textAlign = "center";
-        mContext.fillText(day+". "+date,width/2,textSizeNotToday/2+height/2);
+        mContext.fillText(day+". "+date,width/2,textSizeNotToday/2+(1-0.15)*height/2);//compensate
     }
     return mCanvas;    
 }
+//The json data for the event cell is jsonData = {"event":"Welcome coder!","responsor":"Severide","subEvent":[jsonData]};
+//Date info is not needed in this part;
+function createEventCell(document,eventJson,color){
+    var width = 220;
+    var height = 75;
+    var radius = 12;
+    const strokeWidth = 3;
+    const eventDetailSize = 30;
+    const eventDetailFont = eventDetailSize+"px PingFang SC";
+    const eventResponsorSize = 20;
+    const eventResponsorFont = eventResponsorSize+"px PingFang SC";
+    const responsorTextColor = "#4A90E2";
+    const eventDetail = eventJson.event;
+    const responsor = eventJson.responsor;
+    const subEventJson = eventJson.subEvent;
+    //
+    var mCanvas = document.createElement("CANVAS");
+    mCanvas.width = width;
+    mCanvas.height = height;
+    var mContext = mCanvas.getContext("2d");
+    //
+    height = height-strokeWidth;
+    width = width-strokeWidth;
+    radius = radius-strokeWidth;
+    //
+    mContext.save();
+    mContext.translate(strokeWidth/2,strokeWidth/2);
+    mContext.lineWidth = strokeWidth;
+    mContext.beginPath();
+    mContext.moveTo(radius,0);
+    mContext.lineTo(width-radius,0);
+    mContext.arc(width-radius,radius,radius,-Math.PI/2,0,false);
+    mContext.lineTo(width,height-radius);
+    mContext.arc(width-radius,height-radius,radius,0,Math.PI/2,false);
+    mContext.lineTo(radius,height);
+    mContext.arc(radius,height-radius,radius,Math.PI/2,Math.PI,false);
+    mContext.lineTo(0,radius);
+    mContext.arc(radius,radius,radius,Math.PI,3*Math.PI/2,false);
+    mContext.closePath();
+    mContext.strokeStyle = color;
+    mContext.stroke();
+    //
+    mContext.restore();
+    width = width+strokeWidth;
+    height = height+strokeWidth;
+    //
+    mContext.fillStyle = color;
+    mContext.beginPath();
+    mContext.moveTo(radius,0);
+    mContext.lineTo(width-radius,0);
+    mContext.arc(width-radius,radius,radius,-Math.PI/2,0,false);
+    mContext.lineTo(width,height/2);
+    mContext.lineTo(0,height/2);
+    mContext.arc(radius,radius,radius,Math.PI,3*Math.PI/2,false);
+    mContext.closePath();
+    mContext.fill();
+    //
+    mContext.font = eventDetailFont;
+    mContext.fillStyle = "#FFF";
+    if(width<24+Math.round(mContext.measureText(eventDetail))){
+        const lengthPerChar = Math.round(mContext.measureText(eventDetail))/eventDetail.length;
+        const charNum = Math.ceil((width+24)/lengthPerChar);
+        eventDetail = eventDetail.substring(0,charNum);
+    }
+    mContext.textAlign = "center";
+    mContext.fillText(eventDetail,width/2,(1-0.1)*height/4+eventDetailSize/2);
+    mContext.fillStyle = responsorTextColor;
+    mContext.font = eventResponsorFont;
+    mContext.textDecoration = "underline";
+    mContext.fillText(responsor,width/2,(3-0.35)*height/4+eventResponsorSize/2);
+    return mCanvas;
+}
+
 
 
